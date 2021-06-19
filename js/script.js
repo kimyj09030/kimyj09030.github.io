a0efdf8748a62610eacb68686f85691bb5ca603a
Kimyujin <78829906+kimyj09030@users.noreply.github.com>
2021-06-19 17:53:06 +0900

    19일 학원에서 마지막 잡업

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+$(function(){
+  
+    new fullpage('#fullpage', {
+        //options here
+        anchors:['firstPage', 'secondPage', , 'thirdPage', 'fourthPage'],
+        menu: '#gnb',
+        autoScrolling:true,
+        scrollHorizontally: true,
+        navigation: true,
+    });
+    
+    //methods
+    fullpage_api.setAllowScrolling(true);
+
+})
